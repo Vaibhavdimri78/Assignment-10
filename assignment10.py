c8a9b6b048ce4dc84a3487f6da76889868163ba7
Vaibhavdimri78 <111555559+Vaibhavdimri78@users.noreply.github.com>
2022-08-26 10:36:04 -0700

    Add files via upload

---
+++ assignment10.py
@@ -0,0 +1,35 @@
+#1 
+""""
+n =int(input("Enter the value of n as that many times MySirG will print\n"))
+for i in range(1,n+1):
+    print("my sir G")
+    """
+#2
+"""n = int(input("Enter how many natural numbers to be printed"))
+for i in range(1,n+1):
+    print(i,end=' ')"""
+#3
+"""n = int(input("Enter how many natural numbers to be printed"))
+for i in range(n,0,-1):
+    print(i,end=' ')
+    """
+#4
+"""n = int(input("Enter how many natural odd numbers to be printed"))
+for i in range(1,2*n +1,2):
+    print(i,end=' ')   """
+#5
+"""n = int(input("Enter how many natural odd numbers to be printed in reverse"))
+for i in range(2*n +1,1,-2):
+    print(i,end=' ') """
+#6
+"""n = int(input("Enter how many even natural numbers to be printed"))
+for i in range(2,2*n +1,2):
+    print(i,end=' ')    """
+#8
+n = int(input("enter the natural numbers whoes square is required"))
+for i in range(1,n+1):
+    print(i**2,end=' ')    
+#9
+n = int(input("enter the natural numbers whoes square is required"))
+for i in range(1,n+1):
+    print(i**3,end=' ')        
